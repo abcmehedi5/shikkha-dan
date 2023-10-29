e009459bd1d17c79a103cbcba6ad3ae88752d30e
abcmehedi5 <abcmehedi5@gmail.com>
2023-10-29 19:34:24 +0600

    design navbar

--- src/Pages/Common/Navbar/Navbar.jsx
+++ src/Pages/Common/Navbar/Navbar.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import './Navbar.css'; // Create a CSS file for the Navbar styles
 
 const Navbar = () => {
-    return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
-    );
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <a href="/">Shikkha dan</a>
+      </div>
+      <ul className="nav-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="/courses">Courses</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
