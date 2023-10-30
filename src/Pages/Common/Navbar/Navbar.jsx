f63ee4aa02edefa6c1664611d03cf7c9c2f0020c
abcmehedi5 <abcmehedi5@gmail.com>
2023-10-30 10:27:58 +0600

    navbar responsive

--- src/Pages/Common/Navbar/Navbar.jsx
+++ src/Pages/Common/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import './Navbar.css';
-import { Link } from 'react-router-dom';
-import { FaBars } from 'react-icons/fa';
+import React, { useState } from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -18,11 +18,19 @@ const Navbar = () => {
       <div className="menu-icon" onClick={toggleMenu}>
         <FaBars />
       </div>
-      <ul className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/courses">Courses</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+      <ul className={`nav-links ${isMenuOpen ? "active" : ""}`}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/courses">Courses</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
       </ul>
     </nav>
   );
