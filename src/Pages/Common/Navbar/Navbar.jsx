262249d1c87aca6a7fb11b6c0f81a6e38d3cffb4
abcmehedi5 <abcmehedi5@gmail.com>
2023-10-30 10:25:50 +0600

    navbar responisve

--- src/Pages/Common/Navbar/Navbar.jsx
+++ src/Pages/Common/Navbar/Navbar.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import './Navbar.css'; // Create a CSS file for the Navbar styles
+import React, { useState } from 'react';
+import './Navbar.css';
 import { Link } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';
 
 const Navbar = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="logo">
         <a href="/">Shikkha dan</a>
       </div>
-      <ul className="nav-links">
+      <div className="menu-icon" onClick={toggleMenu}>
+        <FaBars />
+      </div>
+      <ul className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/courses">Courses</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/contact">Contact</Link></li>
-  
       </ul>
     </nav>
   );
